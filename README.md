f05f06845de3c754317b3206167f28e175ffe011
Jimmy Phan <jimmyphan@yahoo-inc.com>
2014-06-09 00:27:05 -0700

    completed check in

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ TipCalculator
 =============
 
 Android class week 1 project
+
+Hours: 9
+
+Required User Stories Completed:
+User is displayed the tip of specified percentage for specified entered amount
+User enters the total amount of the transaction
+User can select between tip amounts (i.e 10%, 15%, 20%)
+Upon selecting tip amount, formatted tip value is displayed
+
+
+Optional User Stories Completed:
+(Optional) User changes the total amount and updated tip is reflected automatically
+(Optional) User can select custom tip percentage if desired
+(Optional) User can select how many ways to split the tip
--- src/com/codepath/example/tipcalculator/TipCalculatorActivity.java
+++ src/com/codepath/example/tipcalculator/TipCalculatorActivity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,35 +16,43 @@ public class TipCalculatorActivity extends Activity {
 
 	// constants
 	public static final double PERCENT = 0.01;
+	public static final String NOT_A_NUMBER = "Is not a number!";
 	
 	// values used for tip calculation
 	public static final double[] TIP_PRESET = { 10, 15, 20 };
 	
-	private Double tipPercentage = TIP_PRESET[0] * PERCENT;
+	private Double tipPercentage = Double.NaN;
+	private Double tipSplit = Double.NaN;
 	
 	// local view references
 	EditText etAmount;
 	EditText etAmountCustom;
+	EditText etSplit;
 	TextView tvTipAmount;
+	TextView tvTipSplit;
 	Button btnTip0;
 	Button btnTip1;
 	Button btnTip2;
 	Button btnTipCustom;
+	Button btnSplit;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tip_calculator);
 		
-		// set the text for the 3 tip buttons
 		etAmount = (EditText) findViewById(R.id.etAmount);
 		etAmountCustom = (EditText) findViewById(R.id.etAmountCustom);
+		etSplit = (EditText) findViewById(R.id.etSplit);
 		tvTipAmount = (TextView) findViewById(R.id.tvTipAmount);
+		tvTipSplit = (TextView) findViewById(R.id.tvTipSplit);
 		btnTip0 = (Button) findViewById(R.id.btnTip0);
 		btnTip1 = (Button) findViewById(R.id.btnTip1);
 		btnTip2 = (Button) findViewById(R.id.btnTip2);
 		btnTipCustom = (Button) findViewById(R.id.btnTipCustom);
+		btnSplit = (Button) findViewById(R.id.btnSplit);
 		
+		// set the text for the 3 tip buttons
 		btnTip0.setText(String.format("%d", (int)TIP_PRESET[0]) + "%" );
 		btnTip1.setText(String.format("%d", (int)TIP_PRESET[1]) + "%" );
 		btnTip2.setText(String.format("%d", (int)TIP_PRESET[2]) + "%" );
@@ -67,10 +76,8 @@ public class TipCalculatorActivity extends Activity {
 			}
 		});
 		
-		// OPTIONAL
-		// custom tip
+		
 		etAmountCustom.addTextChangedListener(new TextWatcher() {
-			
 			@Override
 			public void onTextChanged(CharSequence s, int start, int before, int count) {
 			}
@@ -82,9 +89,10 @@ public class TipCalculatorActivity extends Activity {
 			
 			@Override
 			public void afterTextChanged(Editable s) {
-				//onClick( btnTipCustom );
+				
 			}
 		});
+
 	}
 	
 	/**
@@ -94,7 +102,9 @@ public class TipCalculatorActivity extends Activity {
 	 */
 	public void onClick( View v ) {
 		String tipStr;
+		String splitStr;
 		boolean usingPreSet = true;
+		boolean usingSplit = false;
 		
 		switch( v.getId() ) {
 		case R.id.btnTip0:
@@ -107,6 +117,8 @@ public class TipCalculatorActivity extends Activity {
 			tipPercentage = TIP_PRESET[2];
 			break;
 		case R.id.btnTipCustom:
+			// OPTIONAL
+			// custom tip
 			usingPreSet = false;
 			tipStr = etAmountCustom.getText().toString();
 			if ( tipStr == null || tipStr.isEmpty() ) {
@@ -121,12 +133,20 @@ public class TipCalculatorActivity extends Activity {
 		if ( usingPreSet ) {
 			tipStr = tipPercentage.toString();
 			etAmountCustom.setText(tipStr);
-		}	
+		}
 		
 		calculateTip();
 	}
-	
-	
+
+	/**
+	 * Method to calculate split
+	 * 
+	 * @param v View that triggered the split
+	 */
+	public void onSplit( View v ) {
+		calculateSplit();
+	}
+
 	/**
 	 * Retrieve the value from the edit text.
 	 * Perform any necessary checks.
@@ -138,24 +158,68 @@ public class TipCalculatorActivity extends Activity {
 		
 		if ( amountStr.isEmpty() || amountStr == null ) {
 			tvTipAmount.setText("");
+			tvTipSplit.setText("");
 			return;
 		}
 		
 		Double amount = isNumeric(amountStr);
-		
-		// validate input
-		if ( amount.isNaN() || tipPercentage.isNaN() ) {
-			//Toast.makeText(this, "Is not a number!", Toast.LENGTH_SHORT).show();
-			tvTipAmount.setText("Is not a number!");
-			
+		if ( amount.isNaN() ) {
+			tvTipAmount.setText(NOT_A_NUMBER);
 			return;
 		}
 		
+		// validate input
+		
+		
 		// convert to percentage
 		Double tipAmount = amount * tipPercentage * PERCENT;
-		String tipStr = String.format(Locale.getDefault(), "%.2f", tipAmount);
+		String result = String.format(Locale.getDefault(), "Tip is:  $%.2f", tipAmount);
+		tvTipAmount.setText( result );
 		
-		tvTipAmount.setText( "$" + tipStr );
+		// call for split
+		calculateSplit();
+	}
+
+	
+	/**
+	 * Method to calculate the split for the user
+	 */
+	protected void calculateSplit() {
+		// Retrieve current tip
+		String tipStr = tvTipAmount.getText().toString();
+		String[] tokens = tipStr.split("\\$");
+		if ( tokens.length < 2 ) {
+			return;
+		}
+		// tip value is after the dollar sign
+		Double tip = isNumeric(tokens[1]);
+		if ( tip.isNaN() ) {
+			return;
+		}
+		
+		// Retrieve split
+		String splitStr = etSplit.getText().toString();
+		if ( splitStr == null || splitStr.isEmpty() ) {
+			tvTipSplit.setText("");
+			return;
+		}
+		tipSplit = isNumeric(splitStr);
+		
+		// Retrieve split
+		if ( tipSplit.isNaN() ) {
+			Log.d("INFO", "Tip split is NaN");
+			tvTipSplit.setText(NOT_A_NUMBER);
+			return;
+		}
+		
+		if ( tipSplit <= 0 ) {
+			return;
+		}
+		
+		// both values are valid
+		Double tipAfterSplit = tip / tipSplit;
+		String result = String.format(Locale.getDefault(),  "Split is:  $%.2f", tipAfterSplit );
+		tvTipSplit.setText(result);
 	}
 	
 	/**
